e22415c0c00b91fcc9be59d5ce2c520e6eb1c5dc
Ainharan Subramaniam <ainharan.subramaniam@gmail.com>
2022-09-06 20:00:35 +1000

    feature: moved from init.vim to init.lua

---
+++ nvim/.config/nvim/lua/options/init.lua
@@ -0,0 +1,26 @@
+local set = vim.opt
+
+-- indenting
+set.expandtab = true
+set.smarttab = true
+set.shiftwidth = 4
+set.tabstop = 4
+
+-- search
+set.hlsearch = true
+set.incsearch = true
+set.ignorecase = true
+set.smartcase = true
+
+-- navigation
+set.splitbelow = true
+set.splitright = true
+set.wrap = false
+set.scrolloff = 5
+set.relativenumber = true
+set.cursorline = true
+
+-- misc
+set.fileencoding = 'utf-8'
+set.termguicolors = true
+set.hidden = true

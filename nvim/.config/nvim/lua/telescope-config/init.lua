dff7e3dfb7f72de24e6b1b33d1b14fa80992138e
Ainharan Subramaniam <ainharan.subramaniam@gmail.com>
2022-10-12 11:34:06 +1100

    feature: added fv keybinding for visual search

--- nvim/.config/nvim/lua/telescope-config/init.lua
+++ nvim/.config/nvim/lua/telescope-config/init.lua
@@ -3,3 +3,23 @@ require("telescope").setup({
 		file_ignore_patterns = { "node_modules" },
 	},
 })
+
+-- https://github.com/nvim-telescope/telescope.nvim/issues/1923
+function vim.getVisualSelection()
+  vim.cmd('noau normal! "vy"')
+  local text = vim.fn.getreg('v')
+  vim.fn.setreg('v', {})
+
+  text = string.gsub(text, "\n", "")
+  if #text > 0 then
+    return text
+  else
+    return ''
+  end
+end
+
+local tb = require('telescope.builtin')
+vim.keymap.set('v', '<Leader>fv', function()
+	local visually_selected_text = vim.getVisualSelection()
+	tb.live_grep({ default_text = visually_selected_text })
+end)
